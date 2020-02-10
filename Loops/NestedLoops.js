397bc3392ddbb891327cfec01b0d4d273205eda3
Vlad <hernandez87v@hotmail.com>
2020-02-10 03:30:39 +0000

    Adding 8 files from Loop module and 1 change to Liftoff file.

---
+++ Loops/NestedLoops.js
@@ -0,0 +1,11 @@
+for (var x = 0; x < 3; x = x + 1) {
+  for (var y = 0; y < 2; y = y + 1) {
+    console.log(x + ', ' + y);
+  }
+}
+console.log('===== v 2nd example v =====');
+for (var x = 0; x < 5; x = x + 1) {
+  for (var y = 0; y < 3; y = y + 1) {
+    console.log(x + ',' + y);
+  }
+}
