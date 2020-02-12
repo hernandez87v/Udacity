1257e6d97d9ca4c6bfa4bd4c5124708cecd3ba26
hernandez87v <hernandez87v@hotmail.com>
2020-02-12 00:05:08 +0000

    Quiz: The Price is Right (6-3)

---
+++ Arrays/quiz-ThePriceIsRight-6-3.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.5, 9.99, 1.0, 1.1, 67.0];
+
+// your code goes here
+prices[1] = 55.01;
+prices[3] = 55.02;
+prices[7] = 55.03;
+
+console.log(prices);
