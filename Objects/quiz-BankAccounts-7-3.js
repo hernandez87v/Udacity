6592abe2858649b702b7c3592c0bf4538e9e9a69
hernandez87v <hernandez87v@hotmail.com>
2020-02-16 02:00:25 +0000

    Adding Quiz: Bank Accounts 1 (7-3)

---
+++ Objects/quiz-BankAccounts-7-3.js
@@ -0,0 +1,30 @@
+/*
+ * Programming Quiz: Bank Accounts 1 (7-3)
+ */
+
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  },
+  printAccountSummary: function() {
+    let print =
+      'Welcome!\nYour balance is currently $' +
+      savingsAccount.balance +
+      ' and your interest rate is ' +
+      savingsAccount.interestRatePercent +
+      '%.';
+    return print;
+  }
+};
+
+console.log(savingsAccount.printAccountSummary());
